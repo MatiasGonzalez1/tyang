6be15cd049eee4877c2bca77422b194fc92cf0c9
Matias Gonzalez <mng_epson@hotmail.com>
2023-02-07 21:28:41 -0300

    feat: add method add and del

--- Angular/angular-desde-cero/contador/src/app/app.component.ts
+++ Angular/angular-desde-cero/contador/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contador';
+  count:number = 0;
+  add(){
+   return this.count++
+  }
+
+  restar(){
+    return this.count--
+  }
 }
